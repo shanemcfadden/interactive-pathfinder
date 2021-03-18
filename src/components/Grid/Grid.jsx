84e7c446bb9fdfb6b1c2381b426ca4e0361f26b2
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-18 06:36:59 -0500

    Init Grid

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 import './Grid.css';
 
 const Grid = () => {
-  return <div className="grid">This is a Grid component</div>;
+  const nodes = Array.from({ length: 400 });
+  return (
+    <div className="grid">
+      {nodes.map((val, i) => (
+        <div
+          key={i}
+          style={{
+            border: '1px solid black',
+          }}
+        ></div>
+      ))}
+    </div>
+  );
 };
 
 export default Grid;
