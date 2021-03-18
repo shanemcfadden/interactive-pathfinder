ebec50b4f54a3fab11337f3040f1db3006482c2c
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-18 08:56:42 -0500

    Init grid system

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -3,15 +3,17 @@ import Node from '../Node/Node';
 import './Grid.css';
 
 const Grid = () => {
-  const nodes = Array.from({ length: 400 });
-  nodes[203] = 'start';
-  nodes[204] = 'visited';
-  nodes[179] = 'end';
+  const nodes = Array.from({ length: 20 }, () => Array.from({ length: 20 }));
+  nodes[5][1] = 'start';
+  nodes[5][2] = 'visited';
+  nodes[17][19] = 'end';
   return (
     <div className="grid">
-      {nodes.map((val, i) => (
-        <Node currentState={val} key={i} />
-      ))}
+      {nodes.map((row, i) => {
+        return row.map((val, j) => {
+          return <Node currentState={val} key={`${i}-${j}`} />;
+        });
+      })}
     </div>
   );
 };
