b822ed4f40fea3dddc9b3f62ba8c699e1d819d1e
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-18 06:51:02 -0500

    Init .content-container

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      This is an interactive pathfinder<Grid></Grid>
+      This is an interactive pathfinder
+      <div className="content-container">
+        <Grid></Grid>
+      </div>
     </div>
   );
 }
