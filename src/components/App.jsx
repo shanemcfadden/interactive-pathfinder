c9556a7c48be006bd5281b5377e5ab486a33ef8d
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-03-30 13:22:42 -0500

    Add stateOfPath

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import Dashboard from 'components/Dashboard';
 import Grid from 'components/Grid';
 import 'styles/App.css';
+import { coordinatesAreEqual } from 'util/arr';
 
 function App() {
   // TODO: Make a grid that reflects a neutral state
@@ -11,9 +12,74 @@ function App() {
   );
   const [startNode, setStartNode] = useState([4, 4]);
   const [endNode, setEndNode] = useState([7, 16]);
+  const [stateOfPath, setStateOfPath] = useState(
+    Array.from({ length: 20 }, (row, i) =>
+      Array.from({ length: 20 }, (val, j) => {
+        if (coordinatesAreEqual(startNode, [i, j])) return 3;
+        if (coordinatesAreEqual(endNode, [i, j])) return 4;
+        return 0;
+      })
+    )
+  );
   const [currentClickFunction, setCurrentClickFunction] = useState('none');
   const [drawingWallsAllowed, setDrawingWallsAllowed] = useState(false);
   const [findingPath, setFindingPath] = useState(false);
+
+  const updateStartNode = (coor) => {
+    if (coordinatesAreEqual(coor, endNode)) return;
+    const updatedPath = [...stateOfPath];
+    updatedPath[startNode[0]][startNode[1]] = 0;
+    updatedPath[coor[0]][coor[1]] = 3;
+    setStartNode(coor);
+    setStateOfPath(updatedPath);
+  };
+  const updateEndNode = (coor) => {
+    if (coordinatesAreEqual(coor, startNode)) return;
+    const updatedPath = [...stateOfPath];
+    updatedPath[endNode[0]][endNode[1]] = 0;
+    updatedPath[coor[0]][coor[1]] = 4;
+    setEndNode(coor);
+    setStateOfPath(updatedPath);
+  };
+  const addPathNode = (coor) => {
+    if (
+      coordinatesAreEqual(coor, startNode) ||
+      coordinatesAreEqual(coor, endNode)
+    )
+      return;
+    const updatedPath = [...stateOfPath];
+    updatedPath[coor[0]][coor[1]] = 2;
+    setStateOfPath(updatedPath);
+  };
+  const addVisitedNode = (coor) => {
+    if (
+      coordinatesAreEqual(coor, startNode) ||
+      coordinatesAreEqual(coor, endNode)
+    )
+      return;
+    const updatedPath = [...stateOfPath];
+    updatedPath[coor[0]][coor[1]] = 1;
+    setStateOfPath(updatedPath);
+  };
+  const resetStateOfPath = () => {
+    setStateOfPath(
+      Array.from({ length: 20 }, (row, i) =>
+        Array.from({ length: 20 }, (val, j) => {
+          if (coordinatesAreEqual(startNode, [i, j])) return 3;
+          if (coordinatesAreEqual(endNode, [i, j])) return 4;
+          return 0;
+        })
+      )
+    );
+  };
+  const clearVisitedNodes = () => {
+    const updatedStateOfPath = stateOfPath.map((row) => {
+      return row.map((val) => {
+        return val === 1 ? 0 : val;
+      });
+    });
+    setStateOfPath(updatedStateOfPath);
+  };
 
   useEffect(() => {
     if (drawingWallsAllowed) {
