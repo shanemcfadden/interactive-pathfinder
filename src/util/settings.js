db57ff6b957d6ecb3959efee8afb8948597d35a4
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-04-01 10:34:15 -0500

    Move base settings to top of settings file

--- src/util/settings.js
+++ src/util/settings.js
@@ -19,6 +19,13 @@ export const DEFAULT_END_NODE = [7, 16];
 export const GRID_WIDTH = 20;
 export const GRID_HEIGHT = 20;
 
+export const PATHS_VALUE_NAME_MAP = {
+  1: 'visited',
+  2: 'path',
+  3: 'start',
+  4: 'end',
+};
+
 export const TEXTURES_ARRAY = [
   {
     weight: 1,
@@ -48,12 +55,5 @@ export const TEXTURES_VALUE_NAME_MAP = TEXTURES_ARRAY.reduce(
 export const TEXTURES_NAME_VALUE_MAP = flipKeyValuePairs(
   TEXTURES_VALUE_NAME_MAP
 );
-
-export const PATHS_VALUE_NAME_MAP = {
-  1: 'visited',
-  2: 'path',
-  3: 'start',
-  4: 'end',
-};
 
 export const PATHS_NAME_VALUE_MAP = flipKeyValuePairs(PATHS_VALUE_NAME_MAP);
