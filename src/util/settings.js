bcc7dc07015fa4d9dbba2b1d37f3fdda076c0929
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-04-01 10:31:59 -0500

    Move header content to settings file

--- src/util/settings.js
+++ src/util/settings.js
@@ -1,5 +1,18 @@
 import { flipKeyValuePairs } from './obj';
 
+export const PAGE_HEADER = 'Interactive pathfinder';
+export const PAGE_DESCRIPTION = [
+  `
+    Find the shortest path! Select a starting block and an ending block.
+    To make things trickier, draw some walls for the pathfinder to dodge.
+    Once you press find path, the computer will search for the shortest
+    path using Dijkstra's algorithm.
+  `,
+  `
+    Make a guess, and see if you can beat the computer at its own game!
+  `,
+];
+
 export const DEFAULT_START_NODE = [4, 4];
 export const DEFAULT_END_NODE = [7, 16];
 
