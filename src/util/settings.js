1a52026bed678cac045fdb6704ef5c7dd4b6b127
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-04-06 14:12:38 -0500

    Increase grid dimensions

--- src/util/settings.js
+++ src/util/settings.js
@@ -20,8 +20,8 @@ export const DEFAULT_END_NODE = [7, 16];
 
 export const GRID_GAP_PX = 2;
 export const GRID_WIDTH_PX = 1100;
-export const GRID_MAX_HEIGHT_PX = 600;
-export const GRID_WIDTH_NODES = 30;
+export const GRID_MAX_HEIGHT_PX = 675;
+export const GRID_WIDTH_NODES = 35;
 export const NODE_WIDTH_PX =
   (GRID_WIDTH_PX - GRID_GAP_PX * (GRID_WIDTH_NODES - 1)) / GRID_WIDTH_NODES;
 export const GRID_HEIGHT_NODES = Math.floor(
