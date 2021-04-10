39d6d166d728a760a716f5309588167512d5e8a1
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-04-10 10:33:03 -0500

    Add getShallowCopyIfDefined()

--- src/util/arr.js
+++ src/util/arr.js
@@ -2,6 +2,10 @@ export const coordinatesAreEqual = (coor1, coor2) => {
   return JSON.stringify(coor1) === JSON.stringify(coor2);
 };
 
+export const getShallowCopyIfDefined = (arr) => {
+  return arr ? [...arr] : undefined;
+};
+
 export const shallowCopyOfGrid = (grid) => {
   return grid.map((row) => [...row]);
 };
