c3b5a24b58da11b8f0674cb47e74c823fc74ad4e
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2025-07-30 21:43:33 -0500

    Refactor Select Start and End Buttons

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,40 +1,23 @@
-import { useCallback, type Dispatch, type SetStateAction } from 'react';
+import { type Dispatch, type SetStateAction } from 'react';
 import '../styles/Dashboard.css';
-import Button from './Button';
 import { usePathFindingContext } from '../contexts/PathFindingContext';
-import { useUserActionDispatchContext } from '../contexts/UserActionContext';
 import { SelectTexture } from './SelectTexture';
 import { SelectTerrain } from './SelectTerrain';
 import { ActionButton } from './ActionButton';
+import { SelectStartButton } from './SelectStartButton';
+import { SelectEndButton } from './SelectEndButton';
 
 const Dashboard = ({
   setModalIsOpen,
 }: {
   setModalIsOpen: Dispatch<SetStateAction<boolean>>;
 }) => {
-  const dispatchUserAction = useUserActionDispatchContext();
   const { isFindingPath } = usePathFindingContext();
-
-  const handleStartButtonClick = useCallback(() => {
-    dispatchUserAction({
-      type: 'UPDATE_START_NODE',
-    });
-  }, [dispatchUserAction]);
-
-  const handleEndButtonClick = useCallback(() => {
-    dispatchUserAction({
-      type: 'UPDATE_END_NODE',
-    });
-  }, [dispatchUserAction]);
 
   return (
     <div className="dashboard">
-      <Button onClickFn={handleStartButtonClick} disabled={isFindingPath}>
-        Select Start
-      </Button>
-      <Button onClickFn={handleEndButtonClick} disabled={isFindingPath}>
-        Select End
-      </Button>
+      <SelectStartButton disabled={isFindingPath} />
+      <SelectEndButton disabled={isFindingPath} />
       <SelectTexture disabled={isFindingPath} />
       <SelectTerrain disabled={isFindingPath} />
       <ActionButton setModalIsOpen={setModalIsOpen} />
---
+++ src/components/SelectEndButton.tsx
@@ -0,0 +1,19 @@
+import { useCallback } from 'react';
+import { useUserActionDispatchContext } from '../contexts/UserActionContext';
+import Button from './Button';
+
+export const SelectEndButton = ({ disabled }: { disabled: boolean }) => {
+  const dispatchUserAction = useUserActionDispatchContext();
+
+  const handleEndButtonClick = useCallback(() => {
+    dispatchUserAction({
+      type: 'UPDATE_END_NODE',
+    });
+  }, [dispatchUserAction]);
+
+  return (
+    <Button onClickFn={handleEndButtonClick} disabled={disabled}>
+      Select End
+    </Button>
+  );
+};
