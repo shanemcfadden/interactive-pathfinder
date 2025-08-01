b2e5559227d5e1e904dd1079a89ead0be818fd1b
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2025-08-01 16:05:02 -0500

    Fix build

--- src/contexts/PathFindingContext/constants.ts
+++ src/contexts/PathFindingContext/constants.ts
@@ -7,11 +7,12 @@ import {
 import { Path, type PathValue } from '../../settings/paths';
 import { Grid } from '../../util/grid';
 import { DEFAULT_STARTING_TERRAIN } from '../../settings/terrains';
+import type { PathFindingState } from './types';
 
-export const INITIAL_PATH_FINDING_STATE = {
+export const INITIAL_PATH_FINDING_STATE: PathFindingState = {
   start: DEFAULT_START_NODE,
   end: DEFAULT_END_NODE,
-  isFindingPath: false,
+  isPathFinderActive: false,
   path: new Grid<PathValue>(
     Array.from({ length: GRID_HEIGHT_NODES }, () =>
       Array.from({ length: GRID_WIDTH_NODES }, () => Path.Unvisited),
