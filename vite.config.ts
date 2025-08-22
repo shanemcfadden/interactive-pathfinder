44793a442024f43499396264ea9890f3e0b4fbc3
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2025-08-21 22:58:17 -0500

    Set base path across vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/interactive-pathfinder",
   plugins: [react(), tailwindcss()],
   preview: {
     port: process.env.SERVE_PORT ? parseInt(process.env.SERVE_PORT) : 4173,
